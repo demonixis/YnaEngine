f5764b079538d138d8c853e71ce0a26bb31d4737
Yannick Comte <comte.yannick@gmail.com>
2012-08-17 19:32:05 +0200

    Changes on Kinect plugins (Experimental for now)
    Signed-off-by: Yannick Comte <comte.yannick@gmail.com>

--- Yna/Plugins/Yna.Input.Plugins/Input/Kinect/KinectController.cs
+++ Yna/Plugins/Yna.Input.Plugins/Input/Kinect/KinectController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Kinect;
 using Coding4Fun.Kinect.Wpf;
 using Yna;
@@ -8,35 +9,89 @@ namespace Yna.Input.Kinect
 {
     public class KinectController
     {
+        private static KinectController _instance;
+        private static readonly object _lock = new object();
+
         private bool _isAvailable;
 
-        private float maxX = 0.6f;
+        private float maxX = 0.4f;
         private float maxY = 0.4f;
+        
+        private List<KinectUserProfile> _userProfiles;
 
-        public int HandLeftX = 0;
-        public int HandLeftY = 0;
-        public bool LeftClick = false;
+        private KinectSensor _kinectSensor;
 
-        public int HandRightX = 0;
-        public int HandRightY = 0;
-        public bool RightClick = false;
+        private int _playerCount;
 
-        private KinectSensor kinect;
+        #region Properties
 
+        /// <summary>
+        /// Get or Set the status of the Device
+        /// </summary>
         public bool IsAvailable
         {
             get { return _isAvailable; }
             protected set { _isAvailable = value; }
         }
 
-        public KinectController()
+        /// <summary>
+        /// Number of Kinect device
+        /// </summary>
+        private int Count 
         {
-            int nbKinect = KinectSensor.KinectSensors.Count;
+            get { return KinectSensor.KinectSensors.Count; }
+        }
 
-            if (nbKinect > 0)
+        /// <summary>
+        /// Get the number of players
+        /// </summary>
+        public int PlayerCount 
+        {
+            get { return _playerCount; }
+            protected set { _playerCount = value; }
+        }
+
+        #endregion
+
+        private KinectController()
+        {
+
+        }
+
+        public static KinectController Instance
+        {
+            get
             {
-                kinect = KinectSensor.KinectSensors[0];
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new KinectController();
+                            _instance.Initialize();
+                        }
+                    }
+                }
 
+                return _instance;
+            }
+        }
+
+        private void Initialize()
+        {
+            _playerCount = 0;
+            _userProfiles = new List<KinectUserProfile>(2);
+
+            for (int i = 0; i < 2; i++)
+                _userProfiles[i] = new KinectUserProfile();
+
+            if (Count > 0)
+            {
+                // We take the first (for now)
+                _kinectSensor = KinectSensor.KinectSensors[0];
+
+                // We adjust the defaults parameters for the sensor
                 TransformSmoothParameters parameters = new TransformSmoothParameters
                 {
                     Correction = 0.3f,
@@ -46,38 +101,52 @@ namespace Yna.Input.Kinect
                     Smoothing = 0.7f
                 };
 
-                kinect.SkeletonStream.Enable(parameters);
+                _kinectSensor.SkeletonStream.Enable(parameters);
 
-                kinect.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(kinect_SkeletonFrameReady);
+                _kinectSensor.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(kinect_SkeletonFrameReady);
 
                 try
                 {
-                    kinect.Start();
-                    if (kinect.Status == KinectStatus.Connected)
-                        _isAvailable = true;
+                    _kinectSensor.Start();
+                    _isAvailable = true;
                 }
                 catch (System.IO.IOException ex)
                 {
                     Console.Error.WriteLine(ex.Message);
+                    _isAvailable = false;
                 }
             }
             else
                 _isAvailable = false;
         }
 
-        void kinect_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        public KinectUserProfile GetUserProfile(KinectPlayerIndex index)
+        {
+            return _userProfiles[(int)index];
+        }
+
+        #region Events
+
+        private void kinect_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
         {
             using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
             {
+                // No player
                 if (skeletonFrame == null)
                     return;
 
-                Skeleton[] skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                int skeletonsSize = skeletonFrame.SkeletonArrayLength;
+
+                Skeleton[] skeletons = new Skeleton[skeletonsSize];
 
                 skeletonFrame.CopySkeletonDataTo(skeletons);
 
-                foreach (Skeleton sk in skeletons)
+                int currentPlayer = 0; // Current player
+
+                for (int i = 0; i < skeletonsSize; i++)
                 {
+                    Skeleton sk = skeletons[i];
+
                     if (sk.TrackingState == SkeletonTrackingState.Tracked)
                     {
                         if (sk.Joints[JointType.HandLeft].TrackingState == JointTrackingState.Tracked &&
@@ -89,17 +158,22 @@ namespace Yna.Input.Kinect
                             Joint scaledRight = jointRight.ScaleTo(YnG.DeviceWidth, YnG.DeviceHeight, maxX, maxY);
                             Joint scaledLeft = jointLeft.ScaleTo(YnG.DeviceWidth, YnG.DeviceHeight, maxX, maxY);
 
-                            HandLeftX = (int)scaledLeft.Position.X;
-                            HandLeftY = (int)scaledLeft.Position.Y;
+                            _userProfiles[currentPlayer].SetVector3(scaledLeft.JointType, new Vector3(
+                                scaledLeft.Position.X,
+                                scaledLeft.Position.Y,
+                                scaledLeft.Position.Z));
 
-                            HandRightX = (int)scaledRight.Position.X;
-                            HandRightY = (int)scaledRight.Position.Y;
-
-                            return;
+                            _userProfiles[currentPlayer].SetVector3(scaledRight.JointType, new Vector3(
+                               scaledLeft.Position.X,
+                               scaledLeft.Position.Y,
+                               scaledLeft.Position.Z));
+                            return; // TODO : We must remove that and count the number of human tracked (2 max)
                         }
                     }
                 }
             }
         }
+
+        #endregion
     }
 }
