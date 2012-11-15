84d4c1c8986c9309bb74ab1c9140a73dce5f1197
Drakulo <alex.frene@gmail.com>
2012-11-15 22:50:11 +0100

    Merge correction
    Signed-off-by: Drakulo <alex.frene@gmail.com>

--- Yna/Display/Gui/YnLabel.cs
+++ Yna/Display/Gui/YnLabel.cs
@@ -9,20 +9,47 @@ namespace Yna.Display.Gui
 {
     public class YnLabel : YnWidget
     {
+        #region Protected declarations
+
+        protected string _text;
+        protected Color _textColor;
+        protected bool _useCustomColor;
+        protected SpriteFont _customFont;
+
+        #endregion
+
+        #region Properties
+        
         /// <summary>
         /// The displayed text
         /// </summary>
-        public string Text { get; set; }
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
 
         /// <summary>
         /// The text color. If left empty, the default text color of the skin will be used
         /// </summary>
-        public Color TextColor { get; set; }
+        public Color TextColor
+        {
+            get { return _textColor; }
+            set
+            {
+                _textColor = value;
+                _useCustomColor = true;
+            }
+        }
 
         /// <summary>
         /// Set to true to use a custom color
         /// </summary>
-        public bool UseCustomColor { get; set; }
+        public bool UseCustomColor
+        {
+            get { return _useCustomColor; }
+            set { _useCustomColor = value; }
+        }
 
         /// <summary>
         /// A custom font may be used to display text
@@ -32,22 +59,21 @@ namespace Yna.Display.Gui
             get { return _customFont; }
             set { _customFont = value; }
         }
-        protected SpriteFont _customFont;
 
         /// <summary>
         /// Default constructor
         /// </summary>
         public YnLabel() : base() {
             Text = "";
-            UseCustomColor = false;
+            _useCustomColor = false;
         }
 
         protected override void DrawWidget(GameTime gameTime, SpriteBatch spriteBatch, YnSkin skin)
         {
-            Color color = (UseCustomColor) ? TextColor : skin.DefaultTextColor;
+            Color color = (_useCustomColor) ? _textColor : skin.DefaultTextColor;
             SpriteFont font = (_customFont == null) ? skin.Font : _customFont;
 
-            spriteBatch.DrawString(font, Text, AbsolutePosition, color);
+            spriteBatch.DrawString(font, _text, AbsolutePosition, color);
         }
 
         public override void Layout()
