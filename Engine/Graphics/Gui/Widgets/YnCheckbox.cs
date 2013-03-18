093222ab6d0d6e6047e54540118f82bf965412c1
Drakulo <alex.frene@gmail.com>
2013-03-18 14:12:49 +0100

    Refactor of YnCheckbox
    Signed-off-by: Drakulo <alex.frene@gmail.com>

--- Engine/Graphics/Gui/Widgets/YnCheckbox.cs
+++ Engine/Graphics/Gui/Widgets/YnCheckbox.cs
@@ -9,14 +9,23 @@ using Yna.Engine.Helpers;
 namespace Yna.Engine.Graphics.Gui.Widgets
 {
     /// <summary>
-    /// Basic checkbox widget
+    /// Basic checkbox widget.
     /// </summary>
     public class YnCheckbox : YnButton
     {
-        protected bool _checked;
+        #region Attributes
 
         /// <summary>
-        /// The current check state
+        /// Flag set to true when the box is checked.
+        /// </summary>
+        protected bool _checked;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The current check state.
         /// </summary>
         public bool Checked
         {
@@ -24,6 +33,13 @@ namespace Yna.Engine.Graphics.Gui.Widgets
             set { _checked = value; }
         }
 
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public YnCheckbox()
             : base()
         {
@@ -33,17 +49,39 @@ namespace Yna.Engine.Graphics.Gui.Widgets
             Height = 20;
         }
 
+        /// <summary>
+        /// Constructor with a YnWidgetProperties
+        /// </summary>
+        /// <param name="properties">The properties</param>
+        public YnCheckbox(YnWidgetProperties properties)
+        {
+            SetProperties(properties);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Manage the check flag
+        /// </summary>
+        /// <param name="gameTime">the game time</param>
         protected override void DoCustomUpdate(GameTime gameTime)
         {
             if (_clicked)
             {
+                // Does it really need explainations?
                 _checked = !_checked;
             }
         }
 
+        /// <summary>
+        /// Draw the checked square.
+        /// </summary>
+        /// <param name="gameTime">The game time</param>
+        /// <param name="spriteBatch">The sprite batch</param>
+        /// <param name="skin">The skin to use</param>
         protected override void DrawWidget(GameTime gameTime, SpriteBatch spriteBatch, YnSkin skin)
         {
-        	
+            // Draw the square only if the box is checked
             if (_checked)
             {
                 // Draw a square with padding (Width /5)
@@ -57,6 +95,12 @@ namespace Yna.Engine.Graphics.Gui.Widgets
 
         }
 
+        /// <summary>
+        /// Draw the background.
+        /// </summary>
+        /// <param name="gameTime">The game time</param>
+        /// <param name="spriteBatch">The sprite batch</param>
+        /// <param name="skin">The skin to use</param>
         protected override void DrawBackground(GameTime gameTime, SpriteBatch spriteBatch, YnSkin skin)
         {
             // Always draw the clicked background
